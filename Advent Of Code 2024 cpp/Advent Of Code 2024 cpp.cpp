0223860ca36c08a21c16b37e1736edce90eb0966
Random Typ <no.name.typ2.0@gmail.com>
2024-12-02 23:59:16 +0100

    Finished day2

--- Advent Of Code 2024 cpp/Advent Of Code 2024 cpp.cpp
+++ Advent Of Code 2024 cpp/Advent Of Code 2024 cpp.cpp
@@ -1,9 +1,11 @@
 #include "./src/util.h"
 #include "./src/day1/day1.hpp"
+#include "./src/day2/day2.hpp"
 
 int main()
 {
     day1(readInput(1));
+    day2(readInput(2));
 
     return 0;
 }
---
+++ Advent Of Code 2024 cpp/src/day2/day2.hpp
@@ -0,0 +1,69 @@
+#include "./../util.h"
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <set>
+
+void day2(std::string_view _input) {
+    std::vector<std::string> reports = serializeInput(_input);
+	size_t safeCount = 0;
+
+	for (auto& report : reports)
+	{
+		std::istringstream stream(report);
+		int increase = -1;
+		for (int reading, prev = -1; stream >> reading; prev = reading)
+		{
+			if (prev == -1)
+			{
+				continue;
+			}
+			if (increase == -1)
+			{
+				increase = prev < reading;
+			}
+			int diff = std::abs(prev - reading);
+			if (!diff || diff > 3 || prev < reading != increase)
+			{
+				goto nextReport;
+			}
+		}
+		safeCount++;
+	nextReport:;
+	}
+	std::cout << "Day2: \n \tSafe Count: " << safeCount << "\n";
+	
+	safeCount = 0;
+	for (size_t i = 0; i < reports.size(); i++)
+	{
+		size_t ignoredIndex = 0;
+		tryAgain:
+		std::istringstream stream(reports[i]);
+		int increase = -1;
+		for (int reading, prev = -1, i = 1; stream >> reading; i++)
+		{
+			if (prev == -1)
+			{
+				prev = reading;
+				continue;
+			}
+			if (ignoredIndex == i)
+			{
+				continue;
+			}
+			if (increase == -1)
+			{
+				increase = prev < reading;
+			}
+			int diff = std::abs(prev - reading);
+			if (!diff || diff > 3 || prev < reading != increase)
+			{
+				ignoredIndex++;
+				goto tryAgain;
+			}
+			prev = reading;
+		}
+		safeCount++;
+	}
+	std::cout << "\n\tDampened Safe Count: " << safeCount << "\n";
+}
