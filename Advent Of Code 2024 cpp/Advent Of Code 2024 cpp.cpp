9ce895294cc2f9f147b802dc7e208c327a3b4c74
Random Typ <no.name.typ2.0@gmail.com>
2024-12-04 00:17:35 +0100

    Finished day 3

--- Advent Of Code 2024 cpp/Advent Of Code 2024 cpp.cpp
+++ Advent Of Code 2024 cpp/Advent Of Code 2024 cpp.cpp
@@ -1,11 +1,13 @@
 #include "./src/util.h"
 #include "./src/day1/day1.hpp"
 #include "./src/day2/day2.hpp"
+#include "./src/day3/day3.hpp"
 
 int main()
 {
     day1(readInput(1));
     day2(readInput(2));
+    day3(readInput(3));
 
     return 0;
 }
---
+++ Advent Of Code 2024 cpp/src/day3/day3.hpp
@@ -0,0 +1,27 @@
+#include "./../util.h"
+#include <regex>
+
+void day3(std::string_view _input) {
+	std::smatch color_match;
+	std::regex r("mul\\((\\d+),(\\d+)\\)|do\\(\\)|don't\\(\\)");
+    std::string s(_input);
+	
+    uint64_t acc = 0, acc2 = 0;
+    bool mulEnabled = true;
+    for (std::sregex_iterator i = std::sregex_iterator(s.begin(), s.end(), r), end = std::sregex_iterator(); i != end; i++)
+    {
+		if (i->str().starts_with('d'))
+		{// do or don't
+			mulEnabled = i->str() == "do()";
+			continue;
+		}
+		acc += std::stoi((*i)[1]) * std::stoi((*i)[2]);
+		if (mulEnabled)
+		{
+			acc2 += std::stoi((*i)[1]) * std::stoi((*i)[2]);
+		}
+    }
+
+	std::cout << "Day3: \n \tResult: " << acc << "\n";
+	std::cout << "\n\tAccurate Result: " << acc2 << "\n";
+}
